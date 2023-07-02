101341e24f4236a04ee5c235ef3e82bf31363fdf
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-07-03 01:56:38 +0500

    Added City Selector in Events and MyEvents Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { reset2 } from "../features/event/eventSlice";
 import { getEvents } from "../features/event/eventSlice";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import Card from "../components/Card";
 import Spinner from "../components/Spinner";
 
@@ -12,9 +13,31 @@ const Dashboard = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const btnStyleHover =
+    "w-28 h-10 border-1 rounded-3xl hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 hover:text-white";
+  const btnStyleActive =
+    "w-28 h-10 border-1 rounded-3xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white";
+
+  const [currentCity, setCurrentCity] = useState("All");
+
+  const city_list = ["All", "Karachi", "Lahore", "Islamabad", "Peshawar"];
+
   const { events, isLoading, isError, message } = useSelector(
     (state) => state.event
   );
+
+  const onCitySelection = (e) => {
+    setCurrentCity(e.target.textContent);
+    // e.target.setAttribute("className", btnStyleActive);
+  };
+
+  const btnStyle = (btnCity) => {
+    if (btnCity === currentCity) {
+      return btnStyleActive;
+    } else {
+      return btnStyleHover;
+    }
+  };
 
   const { user } = useSelector((state) => state.auth);
 
@@ -30,7 +53,7 @@ const Dashboard = () => {
     dispatch(getEvents());
 
     dispatch(reset2());
-  }, [user, navigate, isError, message, dispatch]);
+  }, [user, navigate, isError, message, dispatch, currentCity]);
 
   if (isLoading) {
     <Spinner />;
@@ -39,10 +62,21 @@ const Dashboard = () => {
   return (
     <div className="flex flex-col">
       <h1 className="text-center mt-10 font-bold text-2xl">Events</h1>
-      <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
-        {events.map((event) => (
-          <Card event={event} />
+      <div className="flex flex-wrap items-center justify-center my-3 gap-2 px-2">
+        {city_list.map((city) => (
+          <button className={btnStyle(city)} onClick={onCitySelection}>
+            {city}
+          </button>
         ))}
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
+        {events.map((event) => {
+          if (currentCity === "All") {
+            return <Card event={event} />;
+          } else if (currentCity === event.city) {
+            return <Card event={event} />;
+          }
+        })}
       </div>
     </div>
   );
--- src/pages/MyEvents.jsx
+++ src/pages/MyEvents.jsx
@@ -5,6 +5,7 @@ import { reset2, deleteEvent } from "../features/event/eventSlice";
 import { getEvents } from "../features/event/eventSlice";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import Card from "../components/Card";
 import Spinner from "../components/Spinner";
 
@@ -12,9 +13,31 @@ const MyEvents = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const btnStyleHover =
+    "w-28 h-10 border-1 rounded-3xl hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 hover:text-white";
+  const btnStyleActive =
+    "w-28 h-10 border-1 rounded-3xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white";
+
+  const [currentCity, setCurrentCity] = useState("All");
+
+  const city_list = ["All", "Karachi", "Lahore", "Islamabad", "Peshawar"];
+
   const { events, isLoading, isError, message } = useSelector(
     (state) => state.event
   );
+
+  const onCitySelection = (e) => {
+    setCurrentCity(e.target.textContent);
+    // e.target.setAttribute("className", btnStyleActive);
+  };
+
+  const btnStyle = (btnCity) => {
+    if (btnCity === currentCity) {
+      return btnStyleActive;
+    } else {
+      return btnStyleHover;
+    }
+  };
 
   const { user } = useSelector((state) => state.auth);
 
@@ -46,7 +69,7 @@ const MyEvents = () => {
     dispatch(getEvents());
 
     dispatch(reset2());
-  }, [user, navigate, isError, message, dispatch]);
+  }, [user, navigate, isError, message, dispatch, currentCity]);
 
   if (isLoading) {
     <Spinner />;
@@ -55,17 +78,28 @@ const MyEvents = () => {
   return (
     <div className="flex flex-col">
       <h1 className="text-center mt-10 font-bold text-2xl">My Events</h1>
+      <div className="flex flex-wrap items-center justify-center my-3 gap-2 px-2">
+        {city_list.map((city) => (
+          <button className={btnStyle(city)} onClick={onCitySelection}>
+            {city}
+          </button>
+        ))}
+      </div>
       <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
         {events.map((event) => {
           if (event.user.toString() === user._id) {
-            return (
-              <Card
-                event={event}
-                myEvent={true}
-                onDelete={onDelete}
-                onEdit={onEdit}
-              />
-            );
+            if (currentCity === "All") {
+              return (
+                <Card
+                  event={event}
+                  myEvent={true}
+                  onDelete={onDelete}
+                  onEdit={onEdit}
+                />
+              );
+            } else if (currentCity === event.city) {
+              return <Card event={event} />;
+            }
           }
         })}
       </div>
